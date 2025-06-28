13c315110121dcd586f4cdca8f4b2e32da1526a9
nashylyn <natasha.kamau@student.moringaschool,com>
2025-06-28 23:54:02 +0300

    all

--- server/app.py
+++ server/app.py
@@ -14,13 +14,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return jsonify([m.to_dict() for m in messages]), 200
 
-@app.route('/messages/<int:id>')
+    elif request.method == 'POST':
+        data = request.get_json()
+        try:
+            new_message = Message(
+                body=data['body'],
+                username=data['username']
+            )
+            db.session.add(new_message)
+            db.session.commit()
+            return jsonify(new_message.to_dict()), 201
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        if 'body' in data:
+            message.body = data['body']
+            db.session.commit()
+        return jsonify(message.to_dict()), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
+    
